9f7294b8417a3184b96cffe3a82f919e9da88639
Erik Pukinskis <erik@x60erik.(none)>
2010-03-25 18:41:11 -0700

    Added authentication

--- main.rb
+++ main.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'dm-core'
+require 'sinatra-authentication'
+
+use Rack::Session::Cookie, :secret => 'yarr, how many times can a secret be reused? They will never guess the the ~ilde'
 
 class Something
   include DataMapper::Resource
@@ -16,6 +19,10 @@ get '/' do
   haml :index
 end
 
+def name
+  current_user.email.split("@")[0]
+end
+
 __END__
 
 @@ layout
@@ -26,6 +33,18 @@ __END__
     %title hello!
     %link{:rel => 'stylesheet', :type => 'text/css', :href => '/base.css'}
   %body
+    #top_bar
+      %ul#account_links
+        - if logged_in?
+          %li Welcome #{name}!
+          %li
+            %a{:href => '/logout'} Log out
+        - else
+          %li
+            %a{:href => '/login'} Log in
+          %li
+            %a{:href => '/signup'} Sign up
+      #title Name goes here
     = yield
 
 @@ index
